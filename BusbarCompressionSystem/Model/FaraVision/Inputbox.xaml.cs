db142e9f220d483d37ced1d577e2878568cb9760
kzy@faratronic.com.cn <kzy@faratronic.com.cn>
2025-06-29 15:14:37 +0800

    添加对话框

---
+++ BusbarCompressionSystem/Model/FaraVision/Inputbox.xaml.cs
@@ -0,0 +1,70 @@
+﻿using BusbarCompressionSystem.ViewModel;
+using Panuon.WPF.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BusbarCompressionSystem.Model.FaraVision
+{
+    /// <summary>
+    /// Inputbox.xaml 的交互逻辑
+    /// </summary>
+    public partial class Inputbox : WindowX
+    {
+        ViewModelLocator vml = null;
+        public Inputbox()
+        {
+            InitializeComponent();
+            vml = (ViewModelLocator)this.FindResource("Locator");
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+
+            this.DialogResult = true;
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+
+        private void WindowX_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                string dir = $"{vml.Main.DataModel.FaraVisionDataModel.Settingmodel.Prjdir}\\{vml.Main.DataModel.FaraVisionDataModel.Settingmodel.Name}";
+                for (int i = 0; i < 100; i++)
+                {
+                    string filename = $"{dir}\\main{i}.bmp";
+
+                    Tool_Picnumber.Items.Add(i.ToString());
+
+                    if (!File.Exists(filename))
+                    {
+                        break;
+                    }
+                }
+
+                if (Tool_Picnumber.Items.Count > 0)
+                {
+                    Tool_Picnumber.SelectedIndex = Tool_Picnumber.Items.Count - 1;
+                }
+            }
+            catch {; }
+
+        }
+    }
+}
---
+++ BusbarCompressionSystem/Model/FaraVision/newPrj.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Panuon.WPF.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BusbarCompressionSystem.Model
+{
+    /// <summary>
+    /// newPrj.xaml 的交互逻辑
+    /// </summary>
+    public partial class newPrj : WindowX
+    {
+        public string Prj_Name = string.Empty;
+        public newPrj()
+        {
+            InitializeComponent();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string s = Prj_TextBox.Text.Trim().ToUpper();
+                if (!string.IsNullOrEmpty(s))
+                {
+                    Prj_Name = s;
+                    this.DialogResult = true;
+                    return;
+                }
+            }
+            catch (Exception ex) {; }
+
+            NoticeBox.Show("请输入新建工程名称", "提示", MessageBoxIcon.Warning, true, 5000);
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
